3a8aaaa344b53ce27427069826bd5eaf639e3083
Andy Edwards <jedwards@fastmail.com>
2016-09-01 22:49:06 -0500

    get rid of unnecessary meteor shim exists checks

--- src/server/resolveModuleSource.js
+++ src/server/resolveModuleSource.js
@@ -13,13 +13,7 @@ module.exports = function resolveModuleSource(source) {
   const match = /^meteor\/(.*)/.exec(source)
   if (match) {
     const shimFile = path.join(shimDir, match[1] + '.js')
-    let exists
-    try {
-      exists = fs.statSync(shimFile).isFile()
-    } catch (err) {
-      // ignore
-    }
-    if (!exists) fs.writeFileSync(shimFile, 'module.exports = Package.' + match[1].replace(/\//g, '.'))
+    fs.writeFileSync(shimFile, 'module.exports = Package.' + match[1].replace(/\//g, '.'))
     return shimFile
   }
   return source
