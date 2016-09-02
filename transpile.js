0577fdf579f260e9b1d13a54fdb0588070001d8e
Andy Edwards <jedwards@fastmail.com>
2016-09-01 22:43:58 -0500

    clean up transpilation a bit

--- transpile.js
+++ transpile.js
@@ -4,14 +4,31 @@ import promisify from 'es6-promisify'
 import glob from 'glob'
 import mkdirp from 'mkdirp'
 import {transformFile} from 'babel-core'
-import resolveModuleSource from './src/server/resolveModuleSource'
 
-(async () => {
+;(async () => {
   try {
-    const files = await promisify(glob)('src/**/*.js')
+    const shimDir = path.join(__dirname, 'build/server/shims')
+    await promisify(mkdirp)(shimDir)
+
+    let files = await promisify(glob)('src/**/*.js')
+    files = files.filter(file => !/^src\/server\/shims/.test(file) && !/^src\/client/.test(file))
     await Promise.all(files.map(async file => {
       const {code} = await promisify(transformFile)(file, {
-        resolveModuleSource
+        resolveModuleSource(source) {
+          const match = /^meteor\/(.*)/.exec(source)
+          if (match) {
+            const shimFile = path.join(shimDir, match[1] + '.js')
+            let exists
+            try {
+              exists = fs.statSync(shimFile).isFile()
+            } catch (err) {
+              // ignore
+            }
+            if (!exists) fs.writeFileSync(shimFile, 'module.exports = Package.' + match[1].replace(/\//g, '.'))
+            return shimFile
+          }
+          return source
+        }
       })
       const outputFile = path.join(__dirname, 'build', file.substring(3))
       await promisify(mkdirp)(path.dirname(outputFile))
