ead18e7467856d0b572aac587159a4676b074137
Andy Edwards <jedwards@fastmail.com>
2016-09-02 18:32:05 -0500

    launch meteor if necessary before testing for ci's sake

--- run-integration-test.js
+++ run-integration-test.js
@@ -1,7 +1,24 @@
 import phantomjs from 'phantomjs-prebuilt'
+import launchAndWait from './util/launchAndWait'
 import {spawn} from 'child_process'
+import fs from 'fs'
+import path from 'path'
 
-phantomjs.run('--webdriver=4444').then(program => {
+phantomjs.run('--webdriver=4444').then(async program => {
+  let needToRunMeteor = true
+  try {
+    needToRunMeteor = !fs.statSync(path.join(__dirname, 'meteor/.meteor/local/build/programs/server')).isDirectory()
+  } catch (err) {
+    // ignore
+  }
+  if (needToRunMeteor) {
+    const meteor = await launchAndWait('meteor', /App running at: http/i, {
+      cwd: path.join(__dirname, 'meteor')
+    })
+    meteor.kill()
+    meteor.kill('SIGKILL')
+  }
+
   let phantomjsExited = false
   program.on('exit', () => phantomjsExited = true)
   console.log('Started PhantomJS.')
@@ -26,4 +43,7 @@ phantomjs.run('--webdriver=4444').then(program => {
   process.on('exit', kill)
   process.on('SIGINT', kill)
   process.on('SIGTERM', kill)
+}).catch(error => {
+  console.error(error.stack)
+  process.exit(1)
 })
