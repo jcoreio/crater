39928cc223e622886e533b57dfa39b193477d869
Andy Edwards <jedwards@fastmail.com>
2016-09-14 13:33:48 -0500

    move lint and flow to pretest script

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,10 @@
+#!/usr/bin/env babel-node
 // @flow
 
 import phantomjs from 'phantomjs-prebuilt'
 import join from '../util/join'
 import killOnExit from '../util/killOnExit'
 import spawn from '../util/spawn'
-import spawnAsync from '../util/spawnAsync'
 
 import type {ChildProcess} from 'child_process'
 import type {Result} from '../util/join'
@@ -12,9 +12,6 @@ import type {Result} from '../util/join'
 phantomjs.run('--webdriver=4444').then(async (program: ChildProcess): Promise<any> => {
   killOnExit(program)
   console.log('Started PhantomJS.')
-
-  await spawnAsync('npm', ['run', 'flow'], {stdio: 'inherit'})
-  await spawnAsync('npm', ['run', 'lint'], {stdio: 'inherit'})
 
   const wdio = spawn('node_modules/.bin/wdio', [...process.argv.slice(2), 'wdio.conf.js'], {
     stdio: 'inherit'
