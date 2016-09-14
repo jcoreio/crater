31fa424335ae7704c6ba46ffceeebb5f7d47ace9
Andy Edwards <jedwards@fastmail.com>
2016-09-14 10:13:57 -0500

    fix isNewerThan logic and add tests

--- scripts/build-server.js
+++ scripts/build-server.js
@@ -9,10 +9,12 @@ process.on('SIGINT', () => process.exit(1))
 
 const root = path.resolve(__dirname, '..')
 const build = path.join(root, 'build')
+const prerender = path.join(build, 'prerender.js')
 
 asyncScript(async () => {
   await spawnAsync('babel', [path.join(root, 'src', 'index.js'), '-o', path.join(build, 'index.js')], {stdio: 'inherit'})
-  if (await isNewerThan(path.join(root, 'src'), path.join(root, 'build', 'prerender.js'))) {
+  if (await isNewerThan(path.join(root, 'webpack', 'webpack.config.server.js'), prerender) ||
+      await isNewerThan(path.join(root, 'src'), prerender)) {
     console.log('building server bundle...')
     await spawnAsync('webpack', ['--config', path.join(root, 'webpack', 'server.babel.js')], {stdio: 'inherit'})
   } else {
