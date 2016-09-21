32e14b9249e2c732bcdcb56084d512c8cbd5a07a
Andy Edwards <jedwards@fastmail.com>
2016-09-21 10:01:05 -0500

    remove unnecessary check before proxy.ws

--- scripts/devServer.js
+++ scripts/devServer.js
@@ -1,7 +1,6 @@
 // @flow
 
 import express from 'express'
-import url from 'url'
 import webpackConfig from '../webpack/webpack.config.dev'
 
 if (process.env.USE_DOTENV) require('dotenv').config()
@@ -23,11 +22,6 @@ app.all('*', (req: Object, res: Object): any => proxy.web(req, res, { target }))
 
 const server = app.listen(webpackConfig.devServer.port)
 
-server.on('upgrade', (req: Object, socket: any, head: any) => {
-  const parsed = url.parse(req.url)
-  if (parsed && parsed.pathname && /^\/sockjs/.test(parsed.pathname)) {
-    proxy.ws(req, socket, head, { target })
-  }
-})
+server.on('upgrade', (req: Object, socket: any, head: any) => proxy.ws(req, socket, head, { target }))
 
 console.log(`Dev server is listening on http://0.0.0.0:${webpackConfig.devServer.port}`)
