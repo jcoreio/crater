e74fbcf4a0e4649ed2a64bc3d60cb26c03fac181
Andy Edwards <jedwards@fastmail.com>
2016-09-21 10:30:30 -0500

    fix flow error

--- scripts/devServer.js
+++ scripts/devServer.js
@@ -22,6 +22,6 @@ app.all('*', (req: Object, res: Object): any => proxy.web(req, res, { target }))
 
 const server = app.listen(webpackConfig.devServer.port)
 
-server.on('upgrade', (req: Object, socket: any, head: any) => proxy.ws(req, socket, head, { target }))
+server.on('upgrade', (req: Object, socket: any, head: any): any => proxy.ws(req, socket, head, { target }))
 
 console.log(`Dev server is listening on http://0.0.0.0:${webpackConfig.devServer.port}`)
