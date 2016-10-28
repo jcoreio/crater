f165e4abd74b8fb62f84d7afa57edaa68e2470d6
Andy Edwards <jedwards@fastmail.com>
2016-10-27 21:40:31 -0500

    use polling file watchers in CI

--- scripts/start.js
+++ scripts/start.js
@@ -22,6 +22,8 @@ async function start(options?: {commandOptions?: Array<any>} = {}): Promise<any>
   launch({
     commandOptions: options.commandOptions || [],
     main: path.join(src, 'index.js'),
+    usePolling: !!process.env.CI,
+    interval: 1000,
   })
 }
 
