1be0c6c1d3c4598ebc4641a0a6dc16c038a65ce5
Andy Edwards <jedwards@fastmail.com>
2017-06-12 16:36:54 -0500

    fix(PropTypes): use prop-types package instead of React.PropTypes

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {Router, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
 import routes from '../universal/routes/index'
